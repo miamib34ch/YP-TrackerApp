7d03fe1a57d03df139d399109903b4f973ce7f3f
miami <bogdan.polygalov@outlook.com>
2023-04-26 23:43:06 +0500

    Поменял название tabbar'a

--- YP-TrackerApp/SceneDelegate.swift
+++ YP-TrackerApp/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let tabBarViewController = TabBarViewController()
-        window?.rootViewController = tabBarViewController
+        let tabBarController = TabBarController()
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 
--- YP-TrackerApp/TabBarController.swift
+++ YP-TrackerApp/TabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
